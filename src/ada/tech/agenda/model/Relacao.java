b608e787137f3e59a5a7e4f6fec0bff42f2a09d1
Vanessa Rutkoski <nessartk@gmail.com>
2024-09-06 13:27:31 -0300

    Enum de Relacao

--- src/ada/tech/agenda/model/Relacao.java
+++ src/ada/tech/agenda/model/Relacao.java
@@ -1,4 +1,20 @@
 package ada.tech.agenda.model;
 
 public enum Relacao {
+    AMIGO,
+    FILHO,
+    PAI,
+    MAE,
+    PRIMO,
+    COLEGA,
+    CHEFE,
+    VIZINHO,
+    MEDICO,
+    AMANTE,
+    NAMORADO,
+    MARIDO,
+    ESPOSA,
+    OUTRO
+
+
 }
